a83f73b34a658fdfa592467c5b20dbe3f0a98cd6
av_vatni <vatniavdhut14@gmail.com>
2025-04-21 01:15:44 +0530

    modifications in as6

--- as6/as6.php
+++ as6/as6.php
@@ -10,7 +10,8 @@ if($conn->connect_error){
     die("Connection failed".$conn->connect_error);
 }
 
-$stmt = $conn->prepare("INSERT INTO registration (fname, mname, lname, pass) VALUES (?, ?, ?, ?)");
+// Inserting values
+$stmt = $conn->prepare("INSERT INTO registration (fname, mname, lname, password) VALUES (?, ?, ?, ?)");
 $stmt->bind_param("ssss",$fname, $mname, $lname, $pass);
 
 if($stmt->execute()){
@@ -21,13 +22,14 @@ if($stmt->execute()){
 
 $stmt->close();
 
+// Display
 $result = $conn->query("SELECT * FROM registration");
 
 if($result->num_rows > 0){
     while($row = $result->fetch_assoc()){
         echo "ID ". $row["ID"]."|";
-        echo "Name: ".$row["fname"]." ".$row["mname"]." ".$row["lname"];
-        echo "Password".$row["pass"];
+        echo "Name: ".$row["fname"]." ".$row["mname"]." ".$row["lname"]." ";
+        echo "Password: ".$row["password"];
     }
 }else{
     echo "No records";
